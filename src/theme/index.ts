bb936767043773fa48fa7ae30c9257a929181a58
marcelo dato <85354798+mdato@users.noreply.github.com>
2022-05-27 19:59:40 -0300

    Update index.ts

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,27 +1,28 @@
-import { extendTheme, theme as base } from '@chakra-ui/react';
-import { StyleFunctionProps } from '@chakra-ui/theme-tools';
+import { extendTheme, theme as base } from "@chakra-ui/react";
+import { StyleFunctionProps } from "@chakra-ui/theme-tools";
 
 const theme = extendTheme({
   styles: {
     global: {
-      'html, body': {
-        color: 'gray.700',
-        bg:'gray.900',
+      "html, body": {
+        color: "gray.700",
+        bg: "gray.900",
+        background: "gray.900",
       },
     },
   },
   colors: {
     brand: {
-      50: '#f6e8ff',
-      100: '#e3bdff',
-      200: '#cd94ff',
-      300: '#b46ef7',
-      400: '#9a4ce7',
-      500: '#7e31ce',
-      600: '#641eab',
-      700: '#4b1483',
-      800: '#341158',
-      900: '#212121',
+      50: "#f6e8ff",
+      100: "#e3bdff",
+      200: "#cd94ff",
+      300: "#b46ef7",
+      400: "#9a4ce7",
+      500: "#7e31ce",
+      600: "#641eab",
+      700: "#4b1483",
+      800: "#341158",
+      900: "#212121",
     },
   },
   fonts: {
@@ -32,8 +33,8 @@ const theme = extendTheme({
       variants: {
         pill: (props: StyleFunctionProps) => ({
           ...base.components.Button.variants.outline(props),
-          rounded: 'full',
-          color: 'gray.500',
+          rounded: "full",
+          color: "gray.500",
         }),
       },
     },
@@ -41,3 +42,4 @@ const theme = extendTheme({
 });
 
 export default theme;
+
